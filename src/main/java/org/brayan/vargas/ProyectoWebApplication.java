c24652c359e8723801aa9b48b9e483c77fe5f27c
kevinbrayan33o <kevinbrayan33o@gmail.com>
2021-02-23 17:14:53 -0600

    Cambios urgentes jajajaja

--- src/main/java/org/brayan/vargas/ProyectoWebApplication.java
+++ src/main/java/org/brayan/vargas/ProyectoWebApplication.java
@@ -2,7 +2,8 @@ package org.brayan.vargas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
 @SpringBootApplication
 public class ProyectoWebApplication  {
 	
--- src/main/java/org/brayan/vargas/controller/ProductoController.java
+++ src/main/java/org/brayan/vargas/controller/ProductoController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/view/productos")
